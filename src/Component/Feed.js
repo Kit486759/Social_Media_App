c240104c632491287adb3cea6d76adbfef6983dc
kit486759 <kit486759@gmail.com>
2021-06-20 01:19:55 -0700

    Show more or less function

--- src/Component/Feed.js
+++ src/Component/Feed.js
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react'
 import { Context } from './ContextApi'
 import { v4 as uuidv4 } from 'uuid';
 import { FaRegHeart, FaHeart } from "react-icons/fa";
+import Comments from './Comments';
 
 export default function Feed() {
 
@@ -11,7 +12,9 @@ export default function Feed() {
 
     // Submit comment , sent value back to Context
     const submit = (e) => {
-  
+        const userInput = e.target.getElementsByClassName("inputUser")[0]
+        const cmInput = e.target.getElementsByClassName("cmInput")[0]
+        // console.log(userInput)
         e.preventDefault()
         if (input.user === "" || input.comment === "") {
             return alert("Please input all information ")
@@ -19,8 +22,8 @@ export default function Feed() {
         dispatch({ type: "ADD_COMMENT", payload: input })
 
         // Clear user & comment input box after submition
-        e.target.userInput.value = ""
-        e.target.cmInput.value = ""
+        userInput.value = ""
+        cmInput.value = ""
 
     }
 
@@ -34,8 +37,9 @@ export default function Feed() {
             dispatch({ type: "UNLIKED", payload: postId })
     }
 
+
     return (
-        <div className=" relative w-500 mx-auto mt-28">
+        <div className=" relative w-500 mx-auto mt-12">
             {data.map((data, index) => {
                 return (
                     <div className=" relative border border-gray-200 mb-8 h-3/6 mx-auto w-full rounded-md p-2  shadow-md "
@@ -43,7 +47,7 @@ export default function Feed() {
                         <div className="mt-1 mb-2 h-1/6 bg-blue-50">
                             <p className="font-bold text-base">{data.owner.firstName}</p>
                         </div>
-                        <img className=" w-500 h-80 object-cover"
+                        <img className=" w-500 h-80 object-cover select-none"
                             src={data.image} alt="image" />
                         <div className="my-2">
                             {like === false ?
@@ -53,7 +57,7 @@ export default function Feed() {
                                 <FaHeart className="inline" size={20} style={{ cursor: "pointer", color: "af0d0d" }}
                                     onClick={() => likeOnClick(data.id)} />
                             }
-                            <span className="text-sm ml-2 mb-2 align-bottom">{data.likes} Likes </span>
+                            <span className="text-sm ml-2 mb-2 align-bottom select-none">{data.likes} Likes </span>
                         </div>
 
                         <span className="font-black mr-2 text-base mt-5">{data.owner.firstName}</span>
@@ -67,28 +71,18 @@ export default function Feed() {
                         </div>
 
                         {data.cm ?
-                            <>
-                                {data.cm.map((comment, index) => {
-                                    return <div className="my-2 text-sm text"
-                                        key={index}>
-                                        <span className="break-words font-bold mr-2" >{comment.user}</span>
-                                        <span className="break-words">{comment.comment}</span>
-
-                                    </div>
-                                })
-                                }
-                            </>
+                            <Comments data={data} />
                             : null
                         }
 
                         < form onSubmit={submit}>
-                            <input className="block rounded-2xl mb-1 w-full pl-2 focus:outline-none
+                            <input className="inputUser block rounded-2xl mb-1 w-full pl-2 focus:outline-none
                              focus:bg-gray-100 border border-gray-200"
-                                id="userInput" autoComplete="off" placeholder="User" onChange={e => setInput({ ...input, id: data.id, user: e.target.value, uid: uuidv4() })}></input>
+                                autoComplete="off" placeholder="User" onChange={e => setInput({ ...input, id: data.id, user: e.target.value, uid: uuidv4() })}></input>
                             <div className="flex">
-                                <textarea className="rounded-2xl w-10/12 pl-2 pt-2 focus:outline-none
+                                <textarea className="cmInput rounded-2xl w-10/12 pl-2 pt-2 focus:outline-none
                              focus:bg-gray-100 border border-gray-200"
-                                    id="cmInput" autoComplete="off" placeholder="Write comments" onChange={e => setInput({ ...input, comment: e.target.value })}></textarea>
+                                    autoComplete="off" placeholder="Write comments" onChange={e => setInput({ ...input, comment: e.target.value })}></textarea>
                                 <button className="text-gray-400 font-bold p rounded-lg ml-2 w-1/6
                               text-center bg-gray-50 hover:bg-blue-50
                             "
