014640f823fc84f2c4570e748bab24d3ceccc2b4
Kit So <76936712+Kit486759@users.noreply.github.com>
2021-06-20 23:00:07 -0700

    Revert "Daniel"

--- src/Component/Header/PopUp/PopUp.js
+++ src/Component/Header/PopUp/PopUp.js
@@ -1,54 +1,10 @@
-import { useState } from 'react';
-
 import './PopUp.css';
 import { CgProfile } from 'react-icons/cg';
 import { BiImageAdd } from 'react-icons/bi';
 
-const getBase64 = (file) => {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onload = () => resolve(reader.result);
-        reader.onerror = error => reject(error);
-        reader.readAsDataURL(file);
-    });
-}
-
-
 const PopUp = props => {
-    const [img, setImg] = useState([localStorage.getItem('image')]);
-    const [enteredTitle, setEnteredTitle] = useState('');
-
-    const imageUpload = (e) => {
-        const file = e.target.files[0];
-        const fileRes = e.target.result;
-        getBase64(file).then(base64 => {
-            localStorage['image'] = base64;
-            console.debug("file stored", base64);
-            console.log(file);
-        });
-    };
-
     const closeBtn = () => {
         props.close();
-    }
-
-    const onChange = (e) => {
-        e.preventDefault();
-        setEnteredTitle(e.target.value)
-        console.log(enteredTitle);
-    }
-
-    const onSubmit = e => {
-        e.preventDefault();
-        
-        //send enteredTitle
-        if (enteredTitle.trim().length === 0 || img.length === 0) {
-            alert('You must enter a title for your foto!');
-            return;
-        } else {
-            console.log('YOU SUBMITED THIS: ', enteredTitle);
-            closeBtn();
-        }
     }
 
     return (
@@ -68,27 +24,17 @@ const PopUp = props => {
                             <div className='flex'>
                                 <BiImageAdd className='w-10 h-10' />
                             </div>
-                            <input
-                                type="file"
-                                id="imageFile"
-                                name='imageFile'
-                                onChange={imageUpload}
-                            />;
+                                <p>No Image</p>
                         </div>
-                        <form onSubmit={onSubmit}>
-                            <input
-                                type='Text'
-                                name='title'
-                                value={enteredTitle}
-                                onChange={onChange}
-                                placeholder='Title'
-                                className={`border border-black rounded-md mt-4 w-full p-4`}>
-                            </input>
-                            <button type='submit' className='mt-4 bg-black w-full 
+                        <input
+                            type='Text'
+                            placeholder='Title'
+                            className='border border-black rounded-md mt-4 w-full p-4'>
+                        </input>
+                        <button className='mt-4 bg-black w-full 
                         rounded-md border border-black p-2 text-white
                          hover:text-black hover:bg-gray-100'>
-                                Post</button>
-                        </form>
+                            Post</button>
                     </div>
                 </div>
             </div>
